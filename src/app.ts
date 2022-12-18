786ce000a8f34174766e235a9e663a8ce8a7cb02
Josiah-Canada <josiahcanada@yahoo.com>
2022-12-18 12:21:46 -0500

    setup project

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Express }  from 'express';
+import { miamiMotionServer } from './setupServer';
+import databaseConnection from './setupDatabase';
+import { config } from './config';
+
+
+class Application {
+    public initialize(): void{
+        this.loadConfig();
+        const app: Express = express();
+        databaseConnection();
+        const server: miamiMotionServer = new miamiMotionServer(app);
+        server.start();
+    }
+    private loadConfig(): void {
+config.validateConfig();
+    }
+}
+
+const application: Application = new Application();
+application.initialize();
+
+
+
